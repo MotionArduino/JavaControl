50e36050c360175cb28b2797d86b2d6a302b9bcd
caisenchuan <caisenchuan@163.com>
2014-08-25 04:23:10 +0800

    1. Modify README; 2. Modify Tab to Space;

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ JavaControl
 ===========
 
 These code write with java, they receive hand gestures from LeapMotion then turn it into control commands, then send it to arduino via RS232
+
+How to use
+===========
+
+1. These code only support Windows currently, because the RS232 driver we use only support Windows;
+2. Clone the code and import it with eclipse;
+3. Run the MotionXXX java files, like : MotionMeArm;
+4. Every program would ask you to input COM port first, which used to communicate with Arduino;
--- src/RS232.java
+++ src/RS232.java
@@ -15,174 +15,174 @@ import javax.comm.SerialPort;
 import javax.comm.UnsupportedCommOperationException;
 
 public class RS232 {
-	private String PortName;  
-	private CommPortIdentifier portId;  
-	private SerialPort serialPort;  
-	private OutputStream out;
-	private InputStream in;
-	
-	/**
-	 * Load libs
-	 * @return
-	 */
-	static {
-		try {
-			System.loadLibrary("win32com");
-			System.out.println("loadLibrary()...win32com.dll");
-			String driverName = "com.sun.comm.Win32Driver";
-			CommDriver driver = (CommDriver) Class.forName(driverName).newInstance();
-			driver.initialize();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-	} 
-	
-	/**
-	 * Initialize
-	 * @return
-	 */
-	public int open(int PortID) {
-		int openSignal = 1;
-		
-		PortName = "COM" + PortID;
-		System.out.println("Open port : " + PortName);
-		try {
-			portId = CommPortIdentifier.getPortIdentifier(PortName);
-			try {
-				serialPort = (SerialPort) portId.open("Serial_Communication", 2000);
-				System.out.println("Open Serial success!");
-			} catch (PortInUseException e) {
-				System.out.println("error : " + e);
-				e.printStackTrace();
-				
-				if (!portId.getCurrentOwner().equals("Serial_Communication")) {
-					openSignal = 2;
-				} else if (portId.getCurrentOwner().equals("Serial_Communication")) {
-					openSignal = 1;
-					return openSignal;
-				}
-				
-				return openSignal;
-			}
+    private String PortName;  
+    private CommPortIdentifier portId;  
+    private SerialPort serialPort;  
+    private OutputStream out;
+    private InputStream in;
+    
+    /**
+     * Load libs
+     * @return
+     */
+    static {
+        try {
+            System.loadLibrary("win32com");
+            System.out.println("loadLibrary()...win32com.dll");
+            String driverName = "com.sun.comm.Win32Driver";
+            CommDriver driver = (CommDriver) Class.forName(driverName).newInstance();
+            driver.initialize();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    } 
+    
+    /**
+     * Initialize
+     * @return
+     */
+    public int open(int PortID) {
+        int openSignal = 1;
+        
+        PortName = "COM" + PortID;
+        System.out.println("Open port : " + PortName);
+        try {
+            portId = CommPortIdentifier.getPortIdentifier(PortName);
+            try {
+                serialPort = (SerialPort) portId.open("Serial_Communication", 2000);
+                System.out.println("Open Serial success!");
+            } catch (PortInUseException e) {
+                System.out.println("error : " + e);
+                e.printStackTrace();
+                
+                if (!portId.getCurrentOwner().equals("Serial_Communication")) {
+                    openSignal = 2;
+                } else if (portId.getCurrentOwner().equals("Serial_Communication")) {
+                    openSignal = 1;
+                    return openSignal;
+                }
+                
+                return openSignal;
+            }
 
-			// Use InputStream in to read from the serial port, and OutputStream
-			// out to write to the serial port.
-			try {
-				in = serialPort.getInputStream();
-				out = serialPort.getOutputStream();
-				System.out.println("Open stream success!");
-			} catch (IOException e) {
-				openSignal = 3;
-				e.printStackTrace();
-				return openSignal;
-			}
+            // Use InputStream in to read from the serial port, and OutputStream
+            // out to write to the serial port.
+            try {
+                in = serialPort.getInputStream();
+                out = serialPort.getOutputStream();
+                System.out.println("Open stream success!");
+            } catch (IOException e) {
+                openSignal = 3;
+                e.printStackTrace();
+                return openSignal;
+            }
 
-			// Initialize the communication parameters to 9600, 8, 1, none.
-			try {
-				serialPort.setSerialPortParams(9600, 
-											   SerialPort.DATABITS_8,
-											   SerialPort.STOPBITS_1, 
-											   SerialPort.PARITY_NONE);
-				System.out.println("Set params success!");
-			} catch (UnsupportedCommOperationException e) {
-				openSignal = 4;
-				e.printStackTrace();
-				return openSignal;
-			}
-		} catch (NoSuchPortException e) {
-			portId = null;
-			openSignal = 5;
-			e.printStackTrace();
-			return openSignal;
-		}
+            // Initialize the communication parameters to 9600, 8, 1, none.
+            try {
+                serialPort.setSerialPortParams(9600, 
+                                               SerialPort.DATABITS_8,
+                                               SerialPort.STOPBITS_1, 
+                                               SerialPort.PARITY_NONE);
+                System.out.println("Set params success!");
+            } catch (UnsupportedCommOperationException e) {
+                openSignal = 4;
+                e.printStackTrace();
+                return openSignal;
+            }
+        } catch (NoSuchPortException e) {
+            portId = null;
+            openSignal = 5;
+            e.printStackTrace();
+            return openSignal;
+        }
 
-		// when successfully open the serial port, create a new serial buffer,
-		// then create a thread that consistently accepts incoming signals from
-		// the serial port. Incoming signals are stored in the serial buffer.
+        // when successfully open the serial port, create a new serial buffer,
+        // then create a thread that consistently accepts incoming signals from
+        // the serial port. Incoming signals are stored in the serial buffer.
 
-		// return success information
-		
-		return openSignal;
-	}
+        // return success information
+        
+        return openSignal;
+    }
 
-	/**
-	 * Write data to RS232
-	 * @param Msg
-	 */
-	public void write(String Msg) {
-		System.out.println("WritePort : " + Msg);
-		try {
-			if (out != null) {
-				for (int i = 0; i < Msg.length(); i++) {
-					out.write(Msg.charAt(i));
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-	}
+    /**
+     * Write data to RS232
+     * @param Msg
+     */
+    public void write(String Msg) {
+        System.out.println("WritePort : " + Msg);
+        try {
+            if (out != null) {
+                for (int i = 0; i < Msg.length(); i++) {
+                    out.write(Msg.charAt(i));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
-	/**
-	 * Read data from RS232
-	 */
-	public void read() {
-		int c;
-		try {
-			if (in != null) {
-				while (in.available() > 0) {
-					c = in.read();
-					Character d = new Character((char) c);
-					System.out.print(d.toString() + ", ");
-				}
-			}
-			
-			System.out.println("\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/** 
-	* Close RS232
-	*/  
-	public void close() {
-		if(serialPort != null) {
-			serialPort.close();  
-		}
-	}
-	
-	/**
-	 * Test function
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int PORT = 32;
-		RS232 test = new RS232();
-		test.open(PORT);
-		
-		int cnt = 0;
-		while(true) {
-			//String str = String.format("0,0,%s,0,0", cnt);
-			String str = String.format("0,%s,0,0", cnt * 10);
-			test.write(str);
-			if(cnt < 10) {
-				cnt ++;
-			} else {
-				break;
-			}
-			
-			test.read();
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		test.close();
-		
-		// Keep this process running until Enter is pressed
+    /**
+     * Read data from RS232
+     */
+    public void read() {
+        int c;
+        try {
+            if (in != null) {
+                while (in.available() > 0) {
+                    c = in.read();
+                    Character d = new Character((char) c);
+                    System.out.print(d.toString() + ", ");
+                }
+            }
+            
+            System.out.println("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /** 
+    * Close RS232
+    */  
+    public void close() {
+        if(serialPort != null) {
+            serialPort.close();  
+        }
+    }
+    
+    /**
+     * Test function
+     * @param args
+     */
+    public static void main(String[] args) {
+        int PORT = 32;
+        RS232 test = new RS232();
+        test.open(PORT);
+        
+        int cnt = 0;
+        while(true) {
+            //String str = String.format("0,0,%s,0,0", cnt);
+            String str = String.format("0,%s,0,0", cnt * 10);
+            test.write(str);
+            if(cnt < 10) {
+                cnt ++;
+            } else {
+                break;
+            }
+            
+            test.read();
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        test.close();
+        
+        // Keep this process running until Enter is pressed
         System.out.println("Press Enter to quit...");
         try {
             System.in.read();
@@ -191,5 +191,5 @@ public class RS232 {
         }
         
         System.out.println("Exit");
-	}
+    }
 }
